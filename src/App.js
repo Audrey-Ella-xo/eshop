2483bdf8349cdb5395ad3c2ee8562b9ef1f7e448
Odiaka Emmanuella Anita <anitaudrey@gmail.com>
2022-11-29 10:45:41 +0100

    Rouer settings added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import './App.css';
 import Header from "./Header"
 import Home from "./Home"
 import Checkout from "./Checkout"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+      {/* Initialize Router */}
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/checkout" element={<Checkout />} />
+          {/* Home Route MUST come in last!! */}
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </Router>
+
     </div>
   );
 }
