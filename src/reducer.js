bf8af4666ef721e3c7b46a23ed2bd2690a5e14de
Odiaka Emmanuella Anita <anitaudrey@gmail.com>
2022-12-01 14:09:06 +0100

    Add state provider and reducer components

---
+++ src/reducer.js
@@ -0,0 +1,13 @@
+// Here, we'll define all the application level states and actions to make changes to the state
+
+export const initialState = {
+    basket: [],
+}
+
+// Selector
+
+const reducer = (state, action) => {
+
+}
+
+export default reducer
