b83e9e31112dbb9c0a12856eca88e56eba68bc05
Odiaka Emmanuella Anita <anitaudrey@gmail.com>
2023-01-11 15:19:20 +0100

    Made changes to state provider

--- src/StateProvider.js
+++ src/StateProvider.js
@@ -1,15 +1,14 @@
-import React, {createContext, useContext, useReducer} from 'react'
+import React, { createContext, useContext, useReducer } from "react";
 
-
-// Prepare the data layer
+// Prepares the dataLayer
 export const StateContext = createContext();
 
-// Wrap our data and feed the data to the data layer
-export const StateProvider = ({ reducer, initialState, children }) => {
-    <StateContext.Provider value = { useReducer(reducer, initialState)}>
-        {children}
-    </StateContext.Provider>
-}
+// Wrap our app and provide the Data layer
+export const StateProvider = ({ reducer, initialState, children }) => (
+  <StateContext.Provider value={useReducer(reducer, initialState)}>
+    {children}
+  </StateContext.Provider>
+);
 
 // Pull information from the data layer
-export const useStateValue = () => useContext(StateContext)
+export const useStateValue = () => useContext(StateContext);
